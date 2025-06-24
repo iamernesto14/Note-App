fb3d58769d3eeea60d1221deaedb34573a0cd392
iamernesto14 <eernesto211@gmail.com>
2025-06-24 15:49:02 +0000

    feat: implement Note Create page with reactive form and save functionality

--- src/app/components/note-create/note-create.ts
+++ src/app/components/note-create/note-create.ts
@@ -1,11 +1,46 @@
-import { Component } from '@angular/core';
 
-@Component({
-  selector: 'app-note-create',
-  imports: [],
-  templateUrl: './note-create.html',
-  styleUrl: './note-create.scss'
-})
-export class NoteCreate {
+   import { Component } from '@angular/core';
+   import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+   import { NoteService } from '../../services/note';
+   import { Router, RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
-}
+   @Component({
+     selector: 'app-note-create',
+     standalone: true,
+     imports: [ReactiveFormsModule, RouterLink, CommonModule],
+     templateUrl: './note-create.html',
+     styleUrls: ['./note-create.scss']
+   })
+   export class NoteCreate {
+     noteForm: FormGroup;
+     formError: string | null = null;
+
+     constructor(private fb: FormBuilder, private noteService: NoteService, private router: Router) {
+       this.noteForm = this.fb.group({
+         title: ['', Validators.required],
+         content: ['', Validators.required],
+         tags: ['']
+       });
+     }
+
+     onSubmit(): void {
+       if (this.noteForm.valid) {
+         const newNote = this.noteService.create({
+           title: this.noteForm.value.title,
+           content: this.noteForm.value.content,
+           tags: this.noteForm.value.tags.split(',').map((tag: string) => tag.trim()).filter((tag: string) => tag.length > 0) || []
+         });
+         this.formError = null;
+         console.log('Note created:', newNote);
+         this.router.navigate(['/notes']);
+       } else {
+         this.formError = 'Please fill out all required fields';
+       }
+     }
+
+     onCancel(): void {
+       this.router.navigate(['/notes']);
+     }
+   }
+  
