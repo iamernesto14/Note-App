3c573fb31f16496a5e840ca44feac5a84a341869
iamernesto14 <eernesto211@gmail.com>
2025-06-26 15:39:11 +0000

    feat: Add logout button and functionalities

--- src/app/components/shared/sidebar/sidebar.ts
+++ src/app/components/shared/sidebar/sidebar.ts
@@ -1,11 +1,14 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NoteService } from '../../../services/note';
+import { Router } from '@angular/router';
+import { ToasterService } from '../../../services/toaster';
+import { LogoutButton } from '../logout-button/logout-button';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, LogoutButton ], 
   templateUrl: './sidebar.html',
   styleUrls: ['./sidebar.scss']
 })
@@ -17,7 +20,11 @@ export class Sidebar {
   @Output() allNotesSelected = new EventEmitter<void>();
   @Output() archivedNotesSelected = new EventEmitter<void>();
 
-  constructor(private noteService: NoteService) {
+  constructor(
+    private noteService: NoteService,
+    private router: Router,
+    private toasterService: ToasterService
+  ) {
     this.refreshTags();
   }
 
@@ -42,5 +49,11 @@ export class Sidebar {
   onArchivedNotesClick() {
     this.archivedNotesSelected.emit();
     this.activeView = 'archived';
+  }
+
+  onLogoutClick() {
+    localStorage.removeItem('notes'); // Optional: Clear notes
+    this.toasterService.showToast('Successfully logged out!', 'success');
+    this.router.navigate(['/login']);
   }
 }
